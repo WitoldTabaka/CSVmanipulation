63a1c765b78a58db5f4bab78cbbe7e125c855339
WitoldTabaka <wiktorkozak@hotmail.com>
2022-07-11 19:15:50 +0200

    Dodaj pliki projektów.

---
+++ csvCuter/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace csvCuter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "/data.csv";
+            var data = new Program();
+            List<string> Fields = new List<string>
+                { "2", "5"};
+            data.RemoveColumnByIndex(path, Fields);
+        }
+
+        public void RemoveColumnByIndex(string path, List<string> Fields)
+        {
+            List<string> lines = new List<string>();
+            List<int> Index = new List<int>();
+            using (StreamReader reader = new StreamReader(path))
+            {
+                var line = reader.ReadLine();
+                List<string> values = new List<string>();
+                
+                var cols = line.Split(',');
+                for (int i = 0; i < cols.Length; i++)
+                {
+                    for (int w = 0; w < Fields.Count; w++)
+                    {
+                        if (cols[i] == Fields[w])
+                            Index.Add(i);
+                    }
+                }
+
+                while (line != null)
+                {
+                    cols = line.Split(',');
+                    values.Clear();
+                    for (int i = 0; i < cols.Length; i++)
+                    {
+                        for (int w = 0; w < Fields.Count; w++)
+                        {
+                            if (i == Index[w])
+                                values.Add(cols[i]);
+                        }
+                    }
+                    var newLine = string.Join(",", values);
+                    lines.Add(newLine);
+                    line = reader.ReadLine();
+                }
+            }
+
+            using (StreamWriter writer = new StreamWriter(path))
+            {
+                foreach (var line in lines)
+                {
+                    writer.WriteLine(line);
+                }
+            }
+
+        }
+    }
+}
